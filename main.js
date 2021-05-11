13323fc545d592d5722efaa6e5ed0a1d360d25c9
swetharagavendra <42710223+swetharagavendra@users.noreply.github.com>
2021-05-11 14:37:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+menu_list_array = ["Veg Margherita Pizza","Meat Pizza","BBQ Chicken Pizza","Hawaiian Pizza","Pepperoni Pizza"
+                    ];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++)
+{htmldata=htmldata+ '<li>'+ menu_list_array[i] +'</li>'}
+
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML = htmldata;
+
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.push(item);
+menu_list_array.sort();
+htmldata="<section class='cards'>"
+for(var i=0;i<menu_list_array.length;i++)
+{htmldata=htmldata+'<div class="card">' +'<img src="images/pizzaImg.png"/>'+menu_list_array[i];
+}
+htmldata.htmldata+"</section>"
+document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
+
+function add_top(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+}
+
